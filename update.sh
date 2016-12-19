c229154f9a4ddcd7a36664eee9fba44894fc28a5
Patrick <pemami@ufl.edu>
2016-12-19 20:27:04 +0000

    v 1.0

---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Tensorflow
+sudo pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.12.0rc1-cp27-none-linux_x86_64.whl
+# Future for forward compat with Python 3.5
+sudo pip install --upgrade future 
+# gym
+sudo pip install --upgrade gym
+
+
